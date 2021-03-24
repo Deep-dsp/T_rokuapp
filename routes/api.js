194966a62cdcf3719580ee0cbc82c90cdef69070
Deepkumar Patel <dspguru97@gmail.com>
2021-03-24 19:15:03 -0400

    ums-week-10

---
+++ routes/api.js
@@ -0,0 +1,79 @@
+const express = require('express');
+
+const router = express.Router();
+//import sql conncetion
+const connect = require("../config/sqlConfig");
+
+router.get("/", (req, res)=>{
+    //res.json = echo json_encode in PHP
+    res.json({message: "you hit the API file"});
+});
+
+router.get("/index", (req, res)=>{
+    // run sql query here
+    //res.json( query result here )
+    res.json({message: "all users route"});
+});
+
+//Dyncamic route handler that can accept a parameter
+//this equal to $_GET["id"] (req.params.id)
+//we are passing the id via route : /api/movies/20 etc
+router.get("/movies", (req, res)=>{
+    // run sql query here
+
+    connect.getConnection(function(err, connection){
+        if (err) throw err; // not connected!
+        
+        // Use the connection
+        connection.query('SELECT * FROM tbl_movies', function (error, results) {
+            // When done with the connection, release it.
+            connection.release();
+        
+            // Handle error after the release.
+            if (error) throw error;
+
+            res.json(results);
+        });
+    });
+    //res.json( query result here )
+});
+
+
+router.get("/movies/:id", (req, res)=>{
+    // run sql query here
+    connect.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+        
+        // Use the connection
+        connection.query(`SELECT * FROM tbl_movies WHERE movies_id=${req.params.id}`, function (error, results) {
+            // When done with the connection, release it.
+            connection.release();
+        
+            // Handle error after the release.
+            if (error) throw error;
+    
+            res.json(results);
+        });
+    });
+});
+
+router.get("/music", (req, res)=>{
+    // run sql query here
+    connect.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+        
+        // Use the connection
+        connection.query(`SELECT * FROM tbl_music WHERE restrict_flag='0'`, function (error, results) {
+            // When done with the connection, release it.
+            connection.release();
+        
+            // Handle error after the release.
+            if (error) throw error;
+    
+            res.json(results);
+        });
+    });
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+//express router handles incoming requests and directs them 
+const app = express();
+
+const port = process.env.PORT || 5050;
+
+//this catches every route request -> everytime you change your location bar, this function reacts
+//and intercepts the route request
+app.use((req, res, next)=>{
+    console.log('incoming request');
+
+    //this the origin route requests ie. /api/users/
+    next(); //send the user to the route they requested
+})
+
+// use the API route file to handle API route (../api/users, /api/user/id etc)
+app.use("/api", require("./routes/api"));
+app.use("/ums", require("./routes/ums"));
+
+//run the app at the PORT
+app.listen(port, ()=>{
+    console.log(`server is running on ${port}`);
+});
